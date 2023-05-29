80b0092bd01ae62d7a612bb7482025035920352a
chaiqou <lomtadzenikusha@gmail.com>
2023-05-29 14:11:15 +0400

    chore: write migrations

--- database/migrations/2023_05_29_094633_create_tournamets_table.php
+++ database/migrations/2023_05_29_094633_create_tournamets_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tournamets', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->foreignId('admin_id')->constrained('users');
             $table->timestamps();
         });
     }
